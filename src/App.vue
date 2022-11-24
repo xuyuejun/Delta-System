3545bbc16e7494aca34b0e52317cc3110b572a8b
ジョ　エツキン（徐悦鈞） <mfm19201@bene.fit.ac.jp>
2022-11-25 00:09:02 +0800

    引入Element Plus

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/svg/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+
+      <el-button>Button Test</el-button>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/main.js
+++ src/main.js
@@ -8,8 +8,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { setupRouter } from '@/router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const setupApp = async () => {
   const app = createApp(App)
+  app.use(ElementPlus)
   app.use(createPinia())
   await setupRouter(app)
   app.mount('#app')

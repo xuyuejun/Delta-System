c647559581bcc408aad805c41348fd7961990766
ジョ　エツキン（徐悦鈞） <mfm19201@bene.fit.ac.jp>
2022-11-25 15:25:56 +0800

    引入ElementPlusIconsVue

--- src/main.js
+++ src/main.js
@@ -9,8 +9,14 @@ import { setupRouter } from '@/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const setupApp = async () => {
   const app = createApp(App)
+  // Element Plus Icons
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
   app.use(ElementPlus)
   app.use(createPinia())
   await setupRouter(app)
